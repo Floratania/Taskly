2d54b9393172d5a76b9fd96a97b0dce8ba7f382e
Flora Tania <89037370+Floratania@users.noreply.github.com>
2024-05-03 20:14:01 +0300

    Delete taskly directory

--- taskly/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Task, Profile, Activity
-
-admin.site.register(Task)
-admin.site.register(Profile)
-admin.site.register(Activity)
